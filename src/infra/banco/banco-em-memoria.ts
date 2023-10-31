fd914d8b875db4e11602bf2f19187caf9c359cab
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-10-31 08:28:13 -0400

    commit

--- src/infra/banco/banco-em-memoria.ts
+++ src/infra/banco/banco-em-memoria.ts
@@ -4,13 +4,14 @@ type Filme = {
     descricao:string,
     imagem:string
 }
-class BancoEmMemoria{
+export default class BancoEmMemoria{
     public filmes:Filme[] = []
     constructor(){
 
     }
     public salvar(filme:Filme){
         this.filmes.push(filme)
+        return filme
     }
     public listar(){
         return this.filmes
@@ -18,7 +19,5 @@ class BancoEmMemoria{
     public buscarPorId(id:number){
         return this.filmes.find(filme => filme.id === id)
     }
-    public apagarDados(){
-        this.filmes = []
-    }
+   
 }
