8dbadf70740ed2ee90dcd377a8a6a86e7f5a55d6
LuanaDfofa1021b <142016621+LuanaDfofa1021b@users.noreply.github.com>
2023-11-16 08:46:48 -0400

    correção da atividade do dia 14/11

--- src/infra/banco/banco-mongodb.ts
+++ src/infra/banco/banco-mongodb.ts
@@ -1,88 +1,66 @@
 import mongoose from "mongoose";
 import FilmeRepositorioInterface from "../../aplicacao/filme-repositorio-interface";
 require("dotenv").config();
-
-// Interface para representar um documento do MongoDB Filme
-interface FilmeDocument extends mongoose.Document {
-  _id: number;
-  titulo: string;
-  descricao: string;
-  imagem: string;
-}
-
-// Modelo Mongoose para o documento Filme
-const FilmeModelo = mongoose.model<FilmeDocument>(
-  "Filme",
-  new mongoose.Schema({
-    _id: Number,
-    titulo: String,
-    descricao: String,
-    imagem: String,
-  })
-);
-
-// Definição do tipo Filme
-type Filme = {
-  id: number;
-  titulo: string;
-  descricao: string;
-  imagem: string;
-};
-
-export default class BancoMongoDB implements FilmeRepositorioInterface {
-  public filmeModelo: mongoose.Model<FilmeDocument>;
-
+export default class BancoMongoDB implements  FilmeRepositorioInterface {
+  public filmeModelo: any;
   constructor() {
-    try {
-      mongoose.connect(process.env.MONGODB_URL || "");
-      this.filmeModelo = FilmeModelo;
-    } catch (error) {
-      console.log(error);
-      throw new Error("Erro ao conectar com o MongoDB");
+    try{
+      mongoose.connect(process.env.MONGODB_URL || "")
+    }catch(error){
+      console.log(error)
     }
+    this.filmeModelo = mongoose.model("Filme", 
+        new mongoose.Schema({
+            _id: Number,
+            titulo: String,
+            descricao: String,
+            imagem: String
+        })
+    )
   }
-
   async salvar(filme: Filme): Promise<Filme> {
     const filmeDTO = {
       _id: filme.id,
       titulo: filme.titulo,
       descricao: filme.descricao,
-      imagem: filme.imagem,
-    };
-
-    const filmeModelo = new this.filmeModelo(filmeDTO);
-    const result = await filmeModelo.save();
-
-    const filmeSalvo: Filme = {
+      imagem: filme.imagem
+    }
+    const filmeModelo = new this.filmeModelo(filmeDTO)
+    const result =  await filmeModelo.save()
+    const filmeSalvo = {
       id: result._id,
       titulo: result.titulo,
       descricao: result.descricao,
-      imagem: result.imagem,
-    };
-    return filmeSalvo;
+      imagem: result.imagem
+    }
+    return filmeSalvo
   }
-
   async listar(): Promise<Filme[]> {
-    try {
-      const listarFilme = await this.filmeModelo.find().lean(); // Utilize .lean() para obter objetos JavaScript puros
-
-      const filmes: Filme[] = listarFilme.map((filme: FilmeDocument) => ({
+    const listaFilme = await this.filmeModelo.find()
+    return listaFilme.map((filme:FilmeDTO) => {
+      return {
         id: filme._id,
         titulo: filme.titulo,
         descricao: filme.descricao,
-        imagem: filme.imagem,
-      }));
-
-      return filmes;
-    } catch (error) {
-      console.error("Erro ao listar filmes:", error);
-      throw error;
-    }
+        imagem: filme.imagem
+      }
+    })
   }
-
-  async buscarPorId(id: number): Promise<Filme> {
+  async buscarPorId(id:number): Promise<Filme> {
     return new Promise((resolve, reject) => {
       reject(new Error("Not implemented yet"));
-    });
+    })
   }
+}
+type Filme = {
+  id: number,
+  titulo: string,
+  descricao: string,
+  imagem: string
+};
+type FilmeDTO = {
+  _id: number,
+  titulo: string,
+  descricao: string,
+  imagem: string
 }
